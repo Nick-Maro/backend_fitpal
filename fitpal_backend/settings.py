aeccef8a6044c781e70a8f3ce428fa31cccc66c3
nickmaro <nick007sbt@gmail.com>
2024-09-09 22:18:25 +0200

    c

--- fitpal_backend/settings.py
+++ fitpal_backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', 
     'fitness',
-    'register.html'
+
              
 ]
 MIDDLEWARE = [
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'fitpal_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
